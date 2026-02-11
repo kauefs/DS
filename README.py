c35d110f91bd9240b560129d80b179b2db3b5a4f
KAUE <139394184+kauefs@users.noreply.github.com>
2026-02-11 02:21:12 -0300

    README.py

--- README.py
+++ README.py
@@ -28,9 +28,9 @@ try:
         encoded_msg  =urllib.parse.quote(safe_msg  )
         return f'https://img.shields.io/badge/{encoded_label}-{encoded_msg}-{color}?style=flat'
     yoy_color     ='00CD00' if yoy_growth > 0 else 'D22128'
-    badges        =(f"![Arrivals ]({get_url(f'{latest_year} Arrivals',f'{total_latest:.0f}' ,'808080')})\n\n"
-                    f"![YoYgrowth]({get_url( 'YearOverYear Growth'   ,f'{yoy_growth:+.2f}%' , yoy_color)})\n\n"
-                    f"![Recovery ]({get_url( 'COVID-19 Recovery'     ,f'{recovery:+.2f}%'   ,'0077B5')})")
+    badges        =(f"![Arrivals ]({get_url(f'{latest_year} Arrivals',f'{total_latest:.0f}','808080')})\t"
+                    f"![YoYgrowth]({get_url( 'YearOverYear Growth'   ,f'{yoy_growth:.2f}%' , yoy_color)})\t"
+                    f"![Recovery ]({get_url( 'COVID19 Recovery'      ,f'{recovery:.2f}%'   ,'0077B5')})")
 except Exception as e:
     print(f'Calculation Error: {e}')
     exit(1)
